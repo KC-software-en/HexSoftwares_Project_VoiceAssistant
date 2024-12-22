b4f9fd59cfea868bcf74f486dd72f4b2da182dd6
KC_software_en <kisha.cairncross@gmail.com>
2024-12-22 18:11:45 +0200

    Added comments in voice_assistant.py

---
+++ voice_assistant.py
@@ -0,0 +1,44 @@
+# Create a custom voice assistant using Python to personalise and automate tasks according to your
+# needs. Python's versatility makes it an excellent choice for scripting and development, allowing you to build a
+# voice assistant that can compete with the likes of Siri, Alexa, and Google Assistant.
+
+# ask the user for their name
+
+# greet the user with their name by both speaking and writing it out 
+
+# tell the user the the date, time and weather where they are
+# https://openweathermap.org/api
+
+# ask the user what they would like to do
+
+# ask the assistant what is the user's name
+
+# code the various key words to listen for in a request
+
+# search google for an answer 
+# https://pypi.org/project/rpaframework/
+# https://rpaframework.org/
+# https://rpaframework.org/libraries/browser_playwright/
+# use playwright to automate a search alternatively
+
+# request a browser to be opened
+# webbrowser.open
+
+# open youtube to play a song
+
+# say the date
+
+# say the time
+
+# say the weather
+
+# open instagram
+
+# open python IDE
+
+# open VS code
+
+# tell me a joke
+# https://pypi.org/project/Joking/
+
+# listen for the user saying bye
