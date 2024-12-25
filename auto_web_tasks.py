9eca80a57df9c39bbcb04c7a969646d0d74e01e4
KC_software_en <kisha.cairncross@gmail.com>
2024-12-25 23:43:55 +0200

    added a class to automate wiki search in auto_web_tasks.py

---
+++ auto_web_tasks.py
@@ -0,0 +1,39 @@
+# import selenium for automated web searches
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+
+# create a class to conduct an automated wikipedia search with selenium
+class WikiBot():
+    def __init__ (self):        
+        # initiate webdriver 
+        self.driver = webdriver.Edge()
+    
+    # set query as the search parameter of the search method
+    def wiki_search(self, query):
+        """A method to search for information on Wikipedia.
+
+        :param query: A search term for Wikipedia
+        :type query: str
+        """
+        self.query = query
+        # initiate driver
+        self.driver.get(url="https://www.wikipedia.org/")
+        self.driver.implicitly_wait(0.5)
+        # locate search box by inspecting web page elements & copying xpath
+        # https://selenium-python.readthedocs.io/locating-elements.html#locating-elements
+        search_box = self.driver.find_element(By.XPATH, '//*[@id="searchInput"]')
+        # click on search box
+        search_box.click()
+        # send query to search box
+        search_box.send_keys(query)
+        # locate search button 
+        search_button = self.driver.find_element(By.XPATH, '//*[@id="search-form"]/fieldset/button/i')
+        # click on search button to submit query
+        search_button.click()
+
+# create an instance of the class
+info = WikiBot()
+# call the wiki_search method
+info.wiki_search("selenium")
+
